d0b67c877232db96a6de8188e3102504da0c1862
Trevor Clinkenbeard <tclinkenbeard@openai.com>
2026-06-11 16:06:13 -0700

    Add CommitProxy.TransactionSize histogram

--- fdbserver/CommitProxyServer.actor.cpp
+++ fdbserver/CommitProxyServer.actor.cpp
@@ -402,6 +402,7 @@ ACTOR Future<Void> commitBatcher(ProxyCommitData* commitData,
 				when(CommitTransactionRequest req = waitNext(in)) {
 					// WARNING: this code is run at a high priority, so it needs to do as little work as possible
 					int bytes = getBytes(req);
+					commitData->stats.transactionSizeDist->sample(bytes);
 
 					// Drop requests if memory is under severe pressure
 					if (commitData->commitBatchesMemBytesCount + bytes > memBytesLimit) {
